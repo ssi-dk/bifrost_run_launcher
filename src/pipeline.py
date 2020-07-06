582d2551c5b9b319874a2b69b76fb39bb8a783a0
Kim Ng <kimleeng@gmail.com>
2020-07-06 15:23:32 +0200

    debug

--- src/pipeline.py
+++ src/pipeline.py
@@ -139,7 +139,7 @@ def initialize_run(run_name: str, input_folder: str = ".", run_metadata: str = "
     return (run, samples)
 
 
-def convert_to_datetime(text: str, date_formats: list[str] = ['d-%m-%y', '%d.%m.%Y', '%d/%m/%Y']):
+def convert_to_datetime(text: str, date_formats = ['d-%m-%y', '%d.%m.%Y', '%d/%m/%Y']):
     for date_format in date_formats:
         try:
             return datetime.datetime.strptime(text, date_format)
