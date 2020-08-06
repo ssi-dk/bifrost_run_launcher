b86816a097bb034aab9058eb4039ab6520da77cd
Kim Ng <kimleeng@gmail.com>
2020-08-06 10:12:40 +0200

    Added docker ignore to hide keys and updates to dockerfile for dev and testing

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     include_package_data=True,
 
     install_requires=[
-        'bifrostlib==2.0.7'
+        'bifrostlib==2.0.9'
     ]
 )
--- tests/test_1_standard_workflow.py
+++ tests/test_1_standard_workflow.py
@@ -2,8 +2,8 @@ import os
 import pymongo
 import pytest
 import argparse
-import sys
-from bifrost_run_launcher import launcher
+import yaml
+from bifrost_run_launcher import launcher as brl
 
 @pytest.fixture
 def mydb():
@@ -23,14 +23,14 @@ def test_clear_db(mydb):
     col_components.drop()
     col_samples.drop()
     col_runs.drop()
-    
+
 
 def test_install_component(mydb):
     test_clear_db(mydb)
-    args: argparse.Namespace = launcher.parser(["--install"])
-    launcher.run_program(args)
+    args: argparse.Namespace = brl.parser(["--install"])
+    brl.run_program(args)
 
-def test_pipeline(mydb, tmp_path):
+def test_pipeline_no_data(mydb, tmp_path):
     test_install_component(mydb)
     d = tmp_path / "samples"
     d.mkdir()
@@ -38,16 +38,38 @@ def test_pipeline(mydb, tmp_path):
     p.write_text("text")
     p = d / "Sample1_R2.fastq.gz"
     p.write_text("text")
-    args = launcher.parser([
-        "-pre", "examples/pre_script.sh",
-        "-per", "examples/per_sample_script.sh",
-        "-post", "examples/post_script.sh",
-        "-meta", "examples/run_metadata.tsv",
+    # Resources folder is made with Dockerfile in dev mode
+    args = brl.parser([
+        "-pre", "/bifrost_run_launcher/test_resources/pre_script.sh",
+        "-per", "/bifrost_run_launcher/test_resources/per_sample_script.sh",
+        "-post", "/bifrost_run_launcher/test_resources/post_script.sh",
+        "-meta", "/bifrost_run_launcher/test_resources/run_metadata.tsv",
         "-reads", str(d),
         "-name", "test_run",
         "-type", "test"
     ])
-    launcher.run_program(args)
+    brl.run_program(args)
     assert os.path.isfile("run.yaml")
+    assert os.path.isfile("samples.yaml")
+    assert os.path.isfile("run_script.sh")
+
+def test_pipeline_with_data(mydb, tmp_path):
+    test_install_component(mydb)
+    # Resources folder is made with Dockerfile in dev mode
+    args = brl.parser([
+        "-pre", "/bifrost_run_launcher/test_resources/pre_script.sh",
+        "-per", "/bifrost_run_launcher/test_resources/per_sample_script.sh",
+        "-post", "/bifrost_run_launcher/test_resources/post_script.sh",
+        "-meta", "/bifrost_run_launcher/test_resources/run_metadata.tsv",
+        "-reads", "/bifrost_run_launcher/test_resources",
+        "-name", "test_run",
+        "-type", "test"
+    ])
+    brl.run_program(args)
+    assert os.path.isfile("run.yaml")
+    with open("run.yaml", "r") as file_handle:
+        run_doc = yaml.safe_load(file_handle)
+    print(run_doc["samples"])
+    assert(run_doc["samples"][0]["name"] == "S1")
     assert os.path.isfile("samples.yaml")
     assert os.path.isfile("run_script.sh")
